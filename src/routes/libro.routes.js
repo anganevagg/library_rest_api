502b0d9503f0fb7eb84e403801bce01f11939a76
anganevagg <anganevagg@gmail.com>
2020-09-03 09:17:43 -0500

    CRUD agregado

--- src/routes/libro.routes.js
+++ src/routes/libro.routes.js
@@ -7,11 +7,37 @@ const lib = require("mongoose/lib");
 
 /* Rutas para las consultas */
 
-/* Consulta de todos los registros */
+/* CREATE */
+router.post("/", async (req, res)=>{
+	const { codigo_isbn, titulo, autores, categoria, tipo, ficha_bibliografica } = req.body
+	const libro = new Libro({ odigo_isbn, titulo, autores, categoria, tipo, ficha_bibliografica })
+	libro.save()
+	res.json({status:"libro guardado"})
+})
+
+/* READ  */
 router.get("/", async (req, res) => {
 	const libros = await Libro.find();
 	res.json(req.params);
 });
+
+/* UPDATE */
+router.put("/:id", async(req, res)=>{
+	const { codigo_isbn, titulo, autores, categoria, tipo, ficha_bibliografica } = req.body
+	const newLibro = { codigo_isbn, titulo, autores, categoria, tipo, ficha_bibliografica }
+	await Libro.findByIdAndUpdate(req.params.id, newLibro)
+	res.json({
+		status: "Libro actualizado"
+	})
+})
+
+/* DELETE */
+router.delete("/:id", async(req, res)=>{
+	await Libro.findByIdAndRemove(req.params.id)
+	res.json({
+		status:"Libro eliminado"
+	})
+})
 
 /* Consulta dentro de un arreglo */
 router.get("/buscar/autor/:nombre", async (req, res)=>{
@@ -24,5 +50,8 @@ router.get("/buscar/edicion/:edicion",async(req,res)=>{
 	const libros = await Libro.find({'ficha_bibliografica.edicion':req.params.edicion})
 	res.json(libros)
 })
+
+/* Consulta con una condición */
+
 
 module.exports = router;
